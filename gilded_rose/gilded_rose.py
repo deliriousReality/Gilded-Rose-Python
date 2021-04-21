953b80a4d44165f9c408a35b97c05559f7597872
WJC <wj.colley@hotmail.co.uk>
2021-04-21 13:22:24 +0100

    Refactored

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -1,34 +1,39 @@
 class GildedRose(object):
 
+    BRIE = "Aged Brie"
+    CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+    SULFARAS = "Sulfuras, Hand of Ragnaros"
+
     def __init__(self, items):
         self.items = items
 
+    def increase_quality(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+
+    def not_sulfaras(self, item):
+        if item.quality > 0:
+            if item.name != GildedRose.SULFARAS:
+                item.quality = item.quality - 1
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name != GildedRose.BRIE and item.name != GildedRose.CONCERT:
+                self.not_sulfaras(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+                self.increase_quality(item)
+                if item.name == GildedRose.CONCERT:
+                    if item.sell_in < 11:
+                        self.increase_quality(item)
+                    if item.sell_in < 6:
+                        self.increase_quality(item)
+            if item.name != GildedRose.SULFARAS:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                if item.name != GildedRose.BRIE:
+                    if item.name != GildedRose.CONCERT:
+                        self.not_sulfaras(item)
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.increase_quality(item)
